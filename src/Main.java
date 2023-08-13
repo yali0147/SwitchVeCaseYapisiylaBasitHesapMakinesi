fa0f43887aa0afd6eb11795d431067a7a8f9594a
yali <yusufali0147@hotmail.com>
2023-08-14 02:10:15 +0300

    ondalık sayı hatası düzeltildi

--- src/Main.java
+++ src/Main.java
@@ -3,38 +3,40 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hesap Makinesi");
-        int n1,n2,select;
 
         Scanner inp =new Scanner(System.in);
         System.out.print("ilk sayıyı giriniz : ");
-        n1 = inp.nextInt();
+        int n1 = inp.nextInt();
         System.out.print("İkinci sayıyı giriniz : ");
-        n2=inp.nextInt();
+        int n2=inp.nextInt();
 
         System.out.print("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme\nBir işlem seçiniz :");
-        select=inp.nextInt();
+        int select=inp.nextInt();
+
+        double sonuc ;
 
         switch (select){
-                    case 1:
-                        System.out.println("Toplama işleminiz :"+(n1+n2));
-                        break;
-                    case 2:
-                        System.out.println("Çıkarma işleminiz : "+(n1-n2));
-                        break;
-                    case 3:
-                        System.out.println("Çarpma işleminiz : "+(n1*n2));
-                        break;
-                    case 4 :
-                        switch (n2) {
-                            case 0 :
-                                System.out.println("Bir sayı sıfıra bölünemez.");
-                                break;
-                            default:
-                            System.out.println("Bölme işleminiz : " +(n1/n2));
-                        }
-                        break;
-                    default:
-                        System.out.println("Yanlış seçim yaptınız.Lütfen tekrar deneyin");
+            case 1 -> {
+                sonuc = (n1 + n2);
+                System.out.printf("Sonuç : " + ("%.2f"), sonuc);
+            }
+            case 2 -> {
+                sonuc = (n1 - n2);
+                System.out.printf("Sonuç : " + ("%.2f"),sonuc);
+            }
+            case 3 -> {
+                sonuc = (n1 * n2);
+                System.out.printf("Sonuç : " + ("%.2f"),sonuc);
+            }
+            case 4 -> {
+                if (n2 == 0) {
+                    System.out.println("Bir sayı sıfıra bölünemez.");
+                } else {
+                    sonuc = (double) n1 /n2;
+                    System.out.printf("Sonuç : " + ("%.2f"),sonuc);
+                }
+            }
+            default -> System.out.println("Yanlış seçim yaptınız.Lütfen tekrar deneyin");
         }
 
     }
