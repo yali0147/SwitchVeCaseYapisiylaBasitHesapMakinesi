7492ea9a739698bf180361caae3638966b9ee3f2
yali <yusufali0147@hotmail.com>
2023-08-14 15:31:37 +0300

    son hali

--- src/Main.java
+++ src/Main.java
@@ -6,38 +6,30 @@ public class Main {
 
         Scanner inp =new Scanner(System.in);
         System.out.print("ilk sayıyı giriniz : ");
-        int n1 = inp.nextInt();
+        double n1 = inp.nextDouble();
         System.out.print("İkinci sayıyı giriniz : ");
-        int n2=inp.nextInt();
+        double n2=inp.nextDouble();
 
         System.out.print("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme\nBir işlem seçiniz :");
         int select=inp.nextInt();
 
-        double sonuc ;
 
+        double sonuc = 0 ;
         switch (select){
-            case 1 -> {
-                sonuc = (n1 + n2);
-                System.out.printf("Sonuç : " + ("%.2f"), sonuc);
-            }
-            case 2 -> {
-                sonuc = (n1 - n2);
-                System.out.printf("Sonuç : " + ("%.2f"),sonuc);
-            }
-            case 3 -> {
-                sonuc = (n1 * n2);
-                System.out.printf("Sonuç : " + ("%.2f"),sonuc);
-            }
+            case 1 -> sonuc = (n1 + n2);
+            case 2 -> sonuc = (n1 - n2);
+            case 3 -> sonuc = (n1 * n2);
             case 4 -> {
                 if (n2 == 0) {
                     System.out.println("Bir sayı sıfıra bölünemez.");
                 } else {
-                    sonuc = (double) n1 /n2;
-                    System.out.printf("Sonuç : " + ("%.2f"),sonuc);
+                    sonuc = n1 /n2;
                 }
             }
+
             default -> System.out.println("Yanlış seçim yaptınız.Lütfen tekrar deneyin");
         }
+        System.out.printf("Sonuç : " + ("%.2f"), sonuc);
 
     }
 }
